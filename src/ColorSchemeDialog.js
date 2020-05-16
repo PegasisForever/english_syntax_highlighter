fd441d0d9d7e43ab7561ab690aa4293b4ead68af
pegasis <admin@pegasis.site>
2020-05-16 11:06:11 -0400

    add color scheme dialog, preview not done

---
+++ src/ColorSchemeDialog.js
@@ -0,0 +1,136 @@
+import React, {Component} from "react"
+import Dialog, {
+    DialogContent,
+    DialogFooter,
+    DialogButton,
+} from "@material/react-dialog"
+import {delDialog} from "./index"
+import "@material/react-dialog/dist/dialog.css"
+import "@material/react-text-field/dist/text-field.css"
+import TextField, {HelperText, Input} from "@material/react-text-field"
+import {SketchPicker} from "react-color"
+import cloneDeep from "lodash/cloneDeep"
+import {Body1} from "@material/react-typography"
+
+function ColorItem(props) {
+    return (
+        <div className={"color-item"}
+             style={{background: props.selected ? "#00000015" : "transparent"}}
+             onClick={props.onClick}>
+            <span className={"color-block"} style={{background: props.color}}/>
+            <span>{props.name}</span>
+        </div>
+    )
+}
+
+function ColorList(props) {
+    return (
+        <div className={"color-list"}>
+            {props.colors.map((color, index) => <ColorItem
+                key={index}
+                color={color}
+                name={colorNameList[index]}
+                selected={index === props.selectedIndex}
+                onClick={() => props.onSelect(index)}
+            />)}
+        </div>
+    )
+}
+
+const colorNameList = [
+    "adverb",
+    "noun",
+    "adposition",
+    "determiner",
+    "interjection",
+    "particle",
+    "punctuation",
+    "verb",
+    "unknown",
+    "numbers",
+    "conjunction",
+    "adjective",
+]
+
+export class ColorSchemeDialog extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedColorIndex: 0,
+            colorScheme: {
+                name: "New Color Scheme",
+                colors: [
+                    "#f2777a",
+                    "#f99157",
+                    "#ffcc66",
+                    "#99cc99",
+                    "#66cccc",
+                    "#6699cc",
+                    "#f2f0ec",
+                    "#cc99cc",
+                    "#d3d0c8",
+                    "#e8e6df",
+                    "#d27b53",
+                    "#6699cc",
+                ],
+            },
+        }
+        this.updatePreview = this.updatePreview.bind(this)
+    }
+
+
+    updatePreview() {
+
+    }
+
+    render() {
+        return (
+            <Dialog open onClose={() => delDialog()}>
+                <DialogContent>
+                    <TextField label='Name'><Input
+                        value={this.state.colorScheme.name}
+                        onChange={(e) => {
+                            const newScheme = cloneDeep(this.state.colorScheme)
+                            newScheme.name = e.currentTarget.value
+                            this.setState({colorScheme: newScheme})
+                        }}/>
+                    </TextField>
+                    <div className={"color-dialog-main-content"}>
+                        <ColorList
+                            colors={this.state.colorScheme.colors}
+                            selectedIndex={this.state.selectedColorIndex}
+                            onSelect={(index) => this.setState({selectedColorIndex: index})}/>
+                        <SketchPicker
+                            disableAlpha
+                            presetColors={[
+                                "#f2777a",
+                                "#f99157",
+                                "#ffcc66",
+                                "#99cc99",
+                                "#66cccc",
+                                "#6699cc",
+                                "#f2f0ec",
+                                "#cc99cc",
+                                "#d3d0c8",
+                                "#e8e6df",
+                                "#d27b53",
+                                "#6699cc",
+                            ]}
+                            color={this.state.colorScheme.colors[this.state.selectedColorIndex]}
+                            onChangeComplete={(color) => {
+                                const newScheme = cloneDeep(this.state.colorScheme)
+                                newScheme.colors[this.state.selectedColorIndex] = color.hex
+                                this.setState({colorScheme: newScheme})
+                                this.updatePreview()
+                            }}
+                        />
+                    </div>
+                </DialogContent>
+                <DialogFooter>
+                    <DialogButton action='cancel'>Cancel</DialogButton>
+                    <DialogButton action='save' isDefault>Save</DialogButton>
+                </DialogFooter>
+            </Dialog>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,93 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, {Fragment} from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import "@material/react-top-app-bar/dist/top-app-bar.css"
+import "@material/react-material-icon/dist/material-icon.css"
+import "@material/react-fab/dist/fab.css"
+import TopAppBar, {
+    TopAppBarFixedAdjust,
+    TopAppBarRow,
+    TopAppBarSection,
+    TopAppBarTitle,
+} from "@material/react-top-app-bar"
+import "@material/react-typography/dist/typography.css"
+import "@material/react-icon-button/dist/icon-button.css"
+import "@material/react-button/dist/button.css"
+import {Headline5} from "@material/react-typography"
+import MaterialIcon from "@material/react-material-icon"
+import Button from "@material/react-button"
+import {ColorSchemeDialog} from "./ColorSchemeDialog"
+
+class ColorSchemeSection extends React.Component {
+    render() {
+        return (
+            <div>
+                <Headline5>Color Schemes
+                    <Button className={"add-btn"}
+                            icon={<MaterialIcon icon='add'/>}
+                            onClick={() => showDialog(<ColorSchemeDialog/>)}
+                    >
+                        Add
+                    </Button>
+                </Headline5>
+
+            </div>
+        )
+    }
+}
+
+let showDialog
+let delDialog
+
+class SettingsPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dialog: null,
+        }
+        this.showDialog = this.showDialog.bind(this)
+        this.delDialog = this.delDialog.bind(this)
+        showDialog = this.showDialog
+        delDialog = this.delDialog
+    }
+
+    showDialog(dialog) {
+        this.setState({
+            dialog: dialog,
+        })
+    }
+
+
+    delDialog() {
+        this.setState({
+            dialog: null,
+        })
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <TopAppBar fixed>
+                    <TopAppBarRow>
+                        <TopAppBarSection align='start'>
+                            <TopAppBarTitle>English Syntax Highlighter Settings</TopAppBarTitle>
+                        </TopAppBarSection>
+                    </TopAppBarRow>
+                </TopAppBar>
+                <TopAppBarFixedAdjust className={"content"}>
+                    <ColorSchemeSection/>
+                </TopAppBarFixedAdjust>
+                {this.state.dialog}
+            </Fragment>
+        )
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <SettingsPage/>
+    </React.StrictMode>,
+    document.getElementById("root"),
+)
+
+export {showDialog, delDialog}
