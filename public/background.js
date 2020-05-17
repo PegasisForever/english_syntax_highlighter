fa000db289791f53833f72eb0d5b3c39373d91a9
pegasis <admin@pegasis.site>
2020-05-16 23:25:49 -0400

    add two more default color scheme

--- public/background.js
+++ public/background.js
@@ -1,23 +1,68 @@
 /*global chrome*/
 chrome.runtime.onInstalled.addListener(() => {
-    const defaultColorSchemes = [{
-        name: "Default Color Scheme",
-        id: "6c7cb460-97dd-11ea-b882-351ff2ea37df",
-        isDark: true,
-        styles: [
-            {color: "#f2777a", isBold: false},
-            {color: "#f99157", isBold: false},
-            {color: "#ffcc66", isBold: false},
-            {color: "#99cc99", isBold: false},
-            {color: "#66cccc", isBold: false},
-            {color: "#6699cc", isBold: false},
-            {color: "#f2f0ec", isBold: false},
-            {color: "#cc99cc", isBold: false},
-            {color: "#d3d0c8", isBold: false},
-            {color: "#e8e6df", isBold: false},
-            {color: "#d27b53", isBold: false},
-            {color: "#6699cc", isBold: false},
-        ],
-    }]
-    chrome.storage.sync.set({colorSchemes: defaultColorSchemes})
+    const defaultColorSchemes = [
+        {
+            "id": "6c7cb460-97dd-11ea-b882-351ff2ea37df",
+            "isDark": true,
+            "name": "Default Color Scheme",
+            "styles": [
+                {"color": "#f2777a", "isBold": false},
+                {"color": "#f99157", "isBold": false},
+                {"color": "#ffcc66", "isBold": false},
+                {"color": "#99cc99", "isBold": false},
+                {"color": "#66cccc", "isBold": false},
+                {"color": "#6699cc", "isBold": false},
+                {"color": "#f2f0ec", "isBold": false},
+                {"color": "#cc99cc", "isBold": false},
+                {"color": "#d3d0c8", "isBold": false},
+                {"color": "#e8e6df", "isBold": false},
+                {"color": "#d27b53", "isBold": false},
+                {"color": "#6699cc", "isBold": false},
+            ],
+        }, {
+            "id": "5c8ef450-97ec-11ea-a7a9-615bf68b414f",
+            "isDark": false,
+            "name": "Default Color Scheme",
+            "styles": [
+                {"color": "#b955be", "isBold": false},
+                {"color": "#a01120", "isBold": false},
+                {"color": "#bb8902", "isBold": false},
+                {"color": "#58aa58", "isBold": false},
+                {"color": "#058d8d", "isBold": false},
+                {"color": "#2a79ca", "isBold": false},
+                {"color": "#3d2d0e", "isBold": false},
+                {"color": "#4b1ac5", "isBold": false},
+                {"color": "#795b07", "isBold": false},
+                {"color": "#523f02", "isBold": false},
+                {"color": "#d27b53", "isBold": false},
+                {"color": "#215d69", "isBold": false},
+            ],
+        }, {
+            "id": "d2d76930-97ec-11ea-a7a9-615bf68b414f",
+            "isDark": false,
+            "name": "Default Only Noun & Verb Color Scheme",
+            "styles": [
+                {"color": "#000000", "isBold": false},
+                {"color": "#9e0513", "isBold": false},
+                {"color": "#020100", "isBold": false},
+                {"color": "#000000", "isBold": false},
+                {"color": "#000000", "isBold": false},
+                {"color": "#000000", "isBold": false},
+                {"color": "#000000", "isBold": false},
+                {"color": "#005fc3", "isBold": false},
+                {"color": "#000000", "isBold": false},
+                {"color": "#000000", "isBold": false},
+                {"color": "#000000", "isBold": false},
+                {"color": "#000000", "isBold": false},
+            ],
+        },
+    ]
+    const defaultRules = [
+        {
+            id: "65a46cca-97e0-11ea-bb37-0242ac130002",
+            url: "https://me.pegasis.site/*+",
+            colorSchemeId: "6c7cb460-97dd-11ea-b882-351ff2ea37df",
+        },
+    ]
+    chrome.storage.sync.set({colorSchemes: defaultColorSchemes, rules: defaultRules})
 })
--- src/storage.js
+++ src/storage.js
@@ -1,26 +1,64 @@
 /*global chrome*/
 import cloneDeep from "lodash/cloneDeep"
 
-const defaultColorScheme = {
-    name: "Default Color Scheme",
-    id: "6c7cb460-97dd-11ea-b882-351ff2ea37df",
-    isDark: true,
-    styles: [
-        {color: "#f2777a", isBold: false},
-        {color: "#f99157", isBold: false},
-        {color: "#ffcc66", isBold: false},
-        {color: "#99cc99", isBold: false},
-        {color: "#66cccc", isBold: false},
-        {color: "#6699cc", isBold: false},
-        {color: "#f2f0ec", isBold: false},
-        {color: "#cc99cc", isBold: false},
-        {color: "#d3d0c8", isBold: false},
-        {color: "#e8e6df", isBold: false},
-        {color: "#d27b53", isBold: false},
-        {color: "#6699cc", isBold: false},
-    ],
-}
-let colorSchemes = [defaultColorScheme]
+const defaultColorSchemes = [
+    {
+        "id": "6c7cb460-97dd-11ea-b882-351ff2ea37df",
+        "isDark": true,
+        "name": "Default Color Scheme",
+        "styles": [
+            {"color": "#f2777a", "isBold": false},
+            {"color": "#f99157", "isBold": false},
+            {"color": "#ffcc66", "isBold": false},
+            {"color": "#99cc99", "isBold": false},
+            {"color": "#66cccc", "isBold": false},
+            {"color": "#6699cc", "isBold": false},
+            {"color": "#f2f0ec", "isBold": false},
+            {"color": "#cc99cc", "isBold": false},
+            {"color": "#d3d0c8", "isBold": false},
+            {"color": "#e8e6df", "isBold": false},
+            {"color": "#d27b53", "isBold": false},
+            {"color": "#6699cc", "isBold": false},
+        ],
+    }, {
+        "id": "5c8ef450-97ec-11ea-a7a9-615bf68b414f",
+        "isDark": false,
+        "name": "Default Color Scheme",
+        "styles": [
+            {"color": "#b955be", "isBold": false},
+            {"color": "#a01120", "isBold": false},
+            {"color": "#bb8902", "isBold": false},
+            {"color": "#58aa58", "isBold": false},
+            {"color": "#058d8d", "isBold": false},
+            {"color": "#2a79ca", "isBold": false},
+            {"color": "#3d2d0e", "isBold": false},
+            {"color": "#4b1ac5", "isBold": false},
+            {"color": "#795b07", "isBold": false},
+            {"color": "#523f02", "isBold": false},
+            {"color": "#d27b53", "isBold": false},
+            {"color": "#215d69", "isBold": false},
+        ],
+    }, {
+        "id": "d2d76930-97ec-11ea-a7a9-615bf68b414f",
+        "isDark": false,
+        "name": "Default Only Noun & Verb Color Scheme",
+        "styles": [
+            {"color": "#000000", "isBold": false},
+            {"color": "#9e0513", "isBold": false},
+            {"color": "#020100", "isBold": false},
+            {"color": "#000000", "isBold": false},
+            {"color": "#000000", "isBold": false},
+            {"color": "#000000", "isBold": false},
+            {"color": "#000000", "isBold": false},
+            {"color": "#005fc3", "isBold": false},
+            {"color": "#000000", "isBold": false},
+            {"color": "#000000", "isBold": false},
+            {"color": "#000000", "isBold": false},
+            {"color": "#000000", "isBold": false},
+        ],
+    },
+]
+let colorSchemes = cloneDeep(defaultColorSchemes)
 
 let inited = false
 
@@ -55,6 +93,7 @@ export function saveColorScheme(colorScheme) {
         newColorSchemes.push(colorScheme)
     }
 
+    console.log(newColorSchemes)
     colorSchemes = newColorSchemes
     if (chrome.storage) {
         chrome.storage.sync.set({colorSchemes: newColorSchemes})
@@ -72,7 +111,7 @@ export function getColorScheme(id) {
         }
     }
 
-    const newDefaultColorScheme = cloneDeep(defaultColorScheme)
+    const newDefaultColorScheme = cloneDeep(defaultColorSchemes[1])
     newDefaultColorScheme.id = id
     return newDefaultColorScheme
 }
